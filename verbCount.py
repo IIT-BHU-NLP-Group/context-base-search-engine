ce0c0ad1bc2973e9014827722ce280c7a9357bf9
Krishnkant Swarnkar <krishnkant.swarnkar.cse15@itbhu.ac.in>
2017-06-23 21:39:54 +0530

    changed verbcount (stemmer to lemmatizerr

--- verbCount.py
+++ verbCount.py
@@ -2,15 +2,17 @@
 import nltk
 from nltk.stem.porter import *
 from nltk.tokenize import sent_tokenize
-from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+# from nltk.stem import SnowballStemmer
 verbTags = ['VBD','VBG','VBN','VB','VBP','VBZ']
 freqDict = {}
+#sentDict = {}
 import codecs
 text = ""
 
-cities =['Varanasi' , 'Kanpur', 'Goa', 'Nainital', 'Mathura', 'New York', 'Tokyo', 'Darjiling', 'Manali', 'Kullu', 'Ujjain', 'Mumbai', 'Amritsar', 'ooty']
-cities += ['London', 'Barcelona', 'Paris', 'Vatican city', 'Agra', 'Jaipur', 'Banglore', 'Gangtok']
-files = ['1'+city+'.txt' for city in cities]
+cities = [ 'Agra', 'Jaipur', 'Banglore', 'Gangtok','Varanasi','Kanpur', 'Goa', 'Nainital', 'Mathura', 'Darjiling', 'Manali', 'Kullu', 'Ujjain', 'Mumbai', 'Amritsar', 'Ooty','Kolkata', 'Hyderabad','Delhi', 'Guwahati','New_Delhi']
+
+files = ['wiki-'+city+'.txt' for city in cities]
 
 for i in files:
     f = codecs.open(i, 'r', 'utf-8-sig')
@@ -18,8 +20,10 @@ for i in files:
     f.close()
 
 sent_tokenize_list = sent_tokenize(text)
-stemmer = SnowballStemmer("english")
-for x in sent_tokenize_list:
+lemmatizer = WordNetLemmatizer()#stemmer = SnowballStemmer("english")
+for y in sent_tokenize_list:
+    # print y
+    x = y
     text = nltk.word_tokenize(x)
     tagged = nltk.pos_tag(text)
     for i in  range(len(tagged)):
@@ -29,20 +33,27 @@ for x in sent_tokenize_list:
         else:
             nex = ("","NP")   
         if (x[1] in verbTags) and (nex[1] not in verbTags):
-            root = stemmer.stem(x[0])
+            root = lemmatizer.lemmatize(x[0],'v') #stemmer.stem(x[0])
             #print root,x[1],x[0]
             if root in freqDict:
                 freqDict[root] += 1
+                # sentDict[root]  = sentDict[root] + [y] 
             else:
                 freqDict[root] = 1
+                # sentDict[root] = [y]
+            print '*******************************',root,'**************************'
+            # print sentDict[root]
+
 
 tup = freqDict.items()
 #print tup
 def comps(info1):
     return info1[1]
-result = codecs.open("result_new.csv","w",'utf-8-sig')
+result = codecs.open("result_wiki_only.csv","w",'utf-8-sig')
 tup = sorted(tup,key = comps,reverse = True)
 for x in tup:
-    print x
-    result.write(x[0] +","+unicode(x[1])+"\n")
+    #print unicode(x[1])+","+x[0]+"\n"
+    result.write(unicode(x[1])+","+x[0]+"\n")
+    # result.write('***************************'+unicode(x[1])+","+x[0]+'*****************************\n'+'\n'.join(sentDict[x[0]])+'\n\n')
+
 result.close()
